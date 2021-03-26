8f4ccc7bf4352e1131a4d557bb486827643b5026
Trond Trosterud <trond.trosterud@uit.no>
2021-03-26 20:01:22 +0100

    Testfile, so far only with prefixes.

---
+++ devtools/verb_minip.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# script to generate paradigms for generating word forms
+# command, when you are in chr:
+# sh devtools/verb_minip.sh 2SYLL_OD | less
+# sh devtools/verb_minip.sh kihlođ 
+
+
+LOOKUP=$(echo $LOOKUP)
+HLOOKUP=$(echo $HLOOKUP)
+GTLANGS=$(echo $GTLANGS)
+
+
+PATTERN=$1
+L_FILE="in.txt"
+cut -d '!' -f1 src/fst/stems/verbs.lexc | egrep $PATTERN | tr '+' ':' | cut -d ':' -f1>$L_FILE
+
+P_FILE="test/data/testverbprefix.txt"
+# S_FILE="test/data/testverbsuffix.txt" # todo: suffix
+
+for lemma in $(cat $L_FILE);
+do
+ for prefix in $(cat $P_FILE) ; # and suffix in $(cat $S_FILE);
+ do
+#   echo "$prefix)${lemma}" | $LOOKUP $GTLANGS/lang-chr/src/generator-gt-norm.xfst # xfst
+      echo "${prefix}${lemma}+Prs" | $HLOOKUP $GTLANGS/lang-chr/src/generator-gt-norm.hfstol # hfst
+#      echo "${prefix}${lemma}${suffix}" | $HLOOKUP $GTLANGS/lang-chr/src/generator-gt-norm.hfstol # hfst
+ done
+ rm -f $L_FILE
+done
+
