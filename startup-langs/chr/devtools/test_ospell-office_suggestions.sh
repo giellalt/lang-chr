giellalt/lang-tlh

[Template merge - langs/und] Corrected placeholder string for two-letter ISO language code.

devtools/test_ospell-office_suggestions.sh
@@ -8,7 +8,7 @@
 # set -x
 
 # Language being tested, ISO 639-1 code if available:
-GTLANG2=se
+GTLANG2=tlh
 
 # Directory variables:
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"

devtools/test_voikkospell_suggestions.sh
@@ -8,7 +8,7 @@
 # set -x
 
 # Language being tested, ISO 639-1 code if available:
-GTLANG2=__UND2__
+GTLANG2=tlh
 
 # Directory variables:
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"

giellalt/lang-chr

startup-langs/chr/devtools/test_ospell-office_suggestions.sh
@@ -8,7 +8,7 @@
 # set -x
 
 # Language being tested, ISO 639-1 code if available:
-GTLANG2=se
+GTLANG2=chr
 
 # Directory variables:
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"

startup-langs/chr/devtools/test_voikkospell_suggestions.sh
@@ -8,7 +8,7 @@
 # set -x
 
 # Language being tested, ISO 639-1 code if available:
-GTLANG2=__UND2__
+GTLANG2=chr
 
 # Directory variables:
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"