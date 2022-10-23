giellalt/lang-est-x-utee

Added language code.

devtools/test_voikkospell_suggestions.sh
@@ -8,7 +8,7 @@
 # set -x
 
 # Language being tested, ISO 639-1 code if available:
-GTLANG2=__UND2__
+GTLANG2=et
 
 # Directory variables:
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"

giellalt/lang-chr

devtools/test_voikkospell_suggestions.sh
@@ -8,7 +8,7 @@
 # set -x
 
 # Language being tested, ISO 639-1 code if available:
-GTLANG2=__UND2__
+GTLANG2=chr
 
 # Directory variables:
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"