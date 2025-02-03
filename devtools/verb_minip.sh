6598cb374b41f804dd8017d984b653f6c7ee9633
Trond Trosterud <trond.trosterud@uit.no>
2025-02-03 09:25:49 +0100

    hfst

--- devtools/verb_minip.sh
+++ devtools/verb_minip.sh
@@ -13,7 +13,7 @@ GTLANGS=$(echo $GTLANGS)
 
 PATTERN=$1
 L_FILE="in.txt"
-cut -d '!' -f1 src/fst/stems/verbs.lexc | egrep $PATTERN | tr '+' ':' | cut -d ':' -f1>$L_FILE
+cut -d '!' -f1 src/fst/morphology/stems/verbs.lexc | egrep $PATTERN | tr '+' ':' | cut -d ':' -f1>$L_FILE
 
 P_FILE="test/data/testverbprefix.txt"
 # S_FILE="test/data/testverbsuffix.txt" # todo: suffix
@@ -22,9 +22,9 @@ for lemma in $(cat $L_FILE);
 do
  for prefix in $(cat $P_FILE) ; # and suffix in $(cat $S_FILE);
  do
-#   echo "$prefix)${lemma}" | $LOOKUP $GTLANGS/lang-chr/src/generator-gt-norm.xfst # xfst
-      echo "${prefix}${lemma}+Prs" | $HLOOKUP $GTLANGS/lang-chr/src/generator-gt-norm.hfstol # hfst
-#      echo "${prefix}${lemma}${suffix}" | $HLOOKUP $GTLANGS/lang-chr/src/generator-gt-norm.hfstol # hfst
+#   echo "$prefix)${lemma}" | $LOOKUP $GTLANGS/lang-chr/src/fst/generator-gt-norm.xfst # xfst
+      echo "${prefix}${lemma}+Prs" | $HLOOKUP $GTLANGS/lang-chr/src/fst/generator-gt-norm.hfstol # hfst
+#      echo "${prefix}${lemma}${suffix}" | $HLOOKUP $GTLANGS/lang-chr/src/fst/generator-gt-norm.hfstol # hfst
  done
  rm -f $L_FILE
 done
